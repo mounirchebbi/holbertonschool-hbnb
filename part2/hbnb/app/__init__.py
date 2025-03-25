7197e62e4228362a4228c8285aa61da2674dbf34
mounirchebbi <2157@holbertonschool.com>
2025-03-25 18:19:40 +0100

    amenities end points

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +11,9 @@ def create_app():
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+    # Register the amenities namespace
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+
     
     # Additional namespaces for places, reviews, and amenities will be added later
 
--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,7 @@
 # app/services/facade.py
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 from werkzeug.security import generate_password_hash
 
 class HBnBFacade:
@@ -43,6 +44,31 @@ class HBnBFacade:
 
         self.user_repo.update(user_id, update_data)
         return self.get_user(user_id)
+    
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            raise ValueError("Amenity not found")
+        
+        # Update only provided fields
+        update_data = {}
+        for key in ['name', 'description']:
+            if key in amenity_data:
+                update_data[key] = amenity_data[key]
+
+        self.amenity_repo.update(amenity_id, update_data)
+        return self.get_amenity(amenity_id)
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):

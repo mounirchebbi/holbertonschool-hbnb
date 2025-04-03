981300d4635224b48923e19e6efa5c7220316ece
mounirchebbi <2157@holbertonschool.com>
2025-04-03 09:30:18 +0100

    map place model

--- part3/hbnb/app/models/place.py
+++ part3/hbnb/app/models/place.py
@@ -1,38 +1,24 @@
 # app/models/place.py
 from .base_model import BaseModel
+from app.database import db  # Import db from the new module
+
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    __tablename__ = 'places'
+
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, default="")
+    price = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+    def __init__(self, title, description, price, latitude, longitude):
         super().__init__()
         self.title = title
-        self.description = description or ""
+        self.description = description
         self.price = float(price)
         self.latitude = float(latitude)
         self.longitude = float(longitude)
-        self.owner = owner  # Store owner ID
-        self.amenities = []
-        self.reviews = []
-
-    def add_amenity(self, amenity_id):
-        if amenity_id not in self.amenities:
-            self.amenities.append(amenity_id)
-            self.save()
-
-    def remove_amenity(self, amenity_id):
-        if amenity_id in self.amenities:
-            self.amenities.remove(amenity_id)
-            self.save()
-
-    def get_amenities(self):
-        return self.amenities
-
-    def add_review(self, review_id):
-        if review_id not in self.reviews:
-            self.reviews.append(review_id)
-            self.save()
-
-    def get_reviews(self):
-        return self.reviews
 
     def to_dict(self):
         return {
@@ -42,9 +28,6 @@ class Place(BaseModel):
             'price': self.price,
             'latitude': self.latitude,
             'longitude': self.longitude,
-            'owner': self.owner,
-            'amenities': self.amenities,
-            'reviews': self.reviews,
             'created_at': self.created_at.isoformat(),
             'updated_at': self.updated_at.isoformat()
         }

21971cc97bf4cfe8a289e2ef70cbb95830b35a41
mounirchebbi <2157@holbertonschool.com>
2025-04-03 09:37:39 +0100

    add review repo

---
+++ part3/hbnb/app/persistence/review_repository.py
@@ -0,0 +1,8 @@
+# app/persistence/review_repository.py
+
+from app.models.review import Review
+from app.persistence.repository import SQLAlchemyRepository
+
+class ReviewRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(Review)

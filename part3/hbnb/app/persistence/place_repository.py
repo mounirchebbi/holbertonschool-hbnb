7eebceece79394c1c1007668c226a1ae490a9ca6
mounirchebbi <2157@holbertonschool.com>
2025-04-03 09:36:30 +0100

    add place repo

---
+++ part3/hbnb/app/persistence/place_repository.py
@@ -0,0 +1,8 @@
+# app/persistence/place_repository.py
+
+from app.models.place import Place
+from app.persistence.repository import SQLAlchemyRepository
+
+class PlaceRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(Place)

818a1dfc12411414423c103622a68634228e9d9f
mounirchebbi <2157@holbertonschool.com>
2025-04-03 08:36:27 +0100

    map users model

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -1,9 +1,18 @@
 # app/models/user.py
 from .base_model import BaseModel
+from app import db
 from flask_bcrypt import Bcrypt  # Import directly from flask_bcrypt
 bcrypt = Bcrypt()  # Initialize bcrypt here
 
 class User(BaseModel):
+    __tablename__ = 'users'
+
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
     def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = first_name
@@ -11,18 +20,20 @@ class User(BaseModel):
         self.email = email
         self.hash_password(password)  # Hash the password on initialization
         self.is_admin = is_admin
-    
+
     def hash_password(self, password):
-        """Hashes the password before storing it."""
-        #from app import bcrypt  # Import bcrypt here to avoid circulatr import
+        """Hash the password before storing it."""
+         #from app import bcrypt  # Import bcrypt here to avoid circulatr import
+
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
-        """Verifies if the provided password matches the hashed password."""
+        """Verify if the provided password matches the hashed password."""
         #from app import bcrypt  # Import bcrypt here to avoid circulatr import
         return bcrypt.check_password_hash(self.password, password)
 
     def to_dict(self):
+        """Keep this method for compatibility with existing API responses."""
         return {
             'id': self.id,
             'first_name': self.first_name,

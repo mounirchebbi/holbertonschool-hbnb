e606a50dab3a356ac60abc421263abbda6933ba4
mounirchebbi <2157@holbertonschool.com>
2025-04-03 10:41:11 +0100

    add owner_id + relationships

--- part3/hbnb/app/models/place.py
+++ part3/hbnb/app/models/place.py
@@ -1,6 +1,7 @@
 # app/models/place.py
 from .base_model import BaseModel
 from app.database import db  # Import db from the new module
+from .place_amenity import place_amenity
 
 
 class Place(BaseModel):
@@ -11,14 +12,20 @@ class Place(BaseModel):
     price = db.Column(db.Float, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
 
-    def __init__(self, title, description, price, latitude, longitude):
+    # Relationships
+    reviews = db.relationship('Review', backref='place', lazy='dynamic')
+    amenities = db.relationship('Amenity', secondary=place_amenity, backref=db.backref('places', lazy='dynamic'), lazy='dynamic')
+
+    def __init__(self, title, description, price, latitude, longitude, owner_id):
         super().__init__()
         self.title = title
         self.description = description
         self.price = float(price)
         self.latitude = float(latitude)
         self.longitude = float(longitude)
+        self.owner_id = owner_id
 
     def to_dict(self):
         return {
